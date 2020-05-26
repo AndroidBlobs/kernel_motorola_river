787d829323883c10527df77bbf9122f5756c37ae
ShivamKumarJha <jha.shivam3@gmail.com>
2020-05-26 23:10:18 +0530

    Add sound/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -74,7 +74,7 @@ MODULE_LICENSE("GPL");
 MODULE_SUPPORTED_DEVICE("{{Generic,USB Audio}}");
 
 
-static int index[SNDRV_CARDS] = SNDRV_DEFAULT_IDX;	/* Index 0-MAX */
+static int index[SNDRV_CARDS] = SNDRV_DEFAULT_IDX_N;	/* Index 0-MAX */
 static char *id[SNDRV_CARDS] = SNDRV_DEFAULT_STR;	/* ID for this card */
 static bool enable[SNDRV_CARDS] = SNDRV_DEFAULT_ENABLE_PNP;/* Enable this card */
 /* Vendor/product IDs for this card */
@@ -708,6 +708,7 @@ static int usb_audio_probe(struct usb_interface *intf,
 		chip->ctrl_intf = alts;
 
 	chip->txfr_quirk = 0;
+	chip->capture_rate_max = 0;
 	err = 1; /* continue */
 	if (quirk && quirk->ifnum != QUIRK_NO_INTERFACE) {
 		/* need some special handlings */
